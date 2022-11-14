099499c8836d984f44b3f1614aff2ffd1b61005a
Giovanna-89 <jarosinska.jd@gmail.com>
2022-11-15 00:39:12 +0100

    data model - relationships, migrations

---
+++ Fudee/Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Fudee.Models
+{
+    public class Address
+    {
+        [Key]
+        [Display(Name = "Identyfikator:")]
+        public int IdAddress { get; set; }
+
+        [Required(ErrorMessage ="Proszę podać miejscowość:")]
+        [Display(Name = "Miejscowość:")]
+        [MaxLength(30, ErrorMessage = "Nazwa mejscowości nie może przekroczyć 30 znaków")]
+        public string? City { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać ulicę:")]
+        [Display(Name = "Ulica:")]
+        [MaxLength(70, ErrorMessage = "Nazwa ulicy nie może przekroczyć 70 znaków")]
+        public string? Street { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać kod pocztowy:")]
+        [Display(Name = "Kod pocztowy:")]
+        [StringLength(5, ErrorMessage = "Kod pocztowy musi mieć 5 znaków")]
+        public string? PostCode { get; set; }
+
+        public virtual Restaurant? Restaurant { get; set; }
+       
+    }
+}
---
+++ Fudee/Models/Dish.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Fudee.Models
+{
+    public class Dish
+    {
+        [Key]
+        [Display(Name = "Identyfikator:")]
+        public int IdDishes { get; set; }
+
+        [Required]
+        [Display(Name = "Nazwa potrawy:")]
+        [MaxLength(30, ErrorMessage = "Nazwa potrawy nie może przekroczyć 30 znaków")]
+        public string? NameDishes { get; set; }
+
+        [Required]
+        [Display(Name = "Opis potrawy:")]
+        [MaxLength(255, ErrorMessage = "Opis potrawy nie może przekroczyć 255 znaków")]
+        public string? DescriptionDishes { get; set; }
+
+        [Required]
+        [Display(Name = "Cena:")]
+        public float? Price { get; set; }
+
+        [Required]
+        [Display(Name = "Danie restauracji:")]
+        public int IdRestaurant { get; set; }
+        [ForeignKey("IdRestaurant")]
+        public virtual Restaurant? Restaurant { get; set; }
+    }
+}
+
